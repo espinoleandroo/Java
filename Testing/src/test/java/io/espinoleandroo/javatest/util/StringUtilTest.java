2a905e65791647e485c001177f8e78255b8d899e
espinoleandroo <espinoleandroo@gmail.com>
2021-08-08 00:45:38 -0500

    Java Testing -- Test unitario lanzando excepcion

--- Testing/src/test/java/io/espinoleandroo/javatest/util/StringUtilTest.java
+++ Testing/src/test/java/io/espinoleandroo/javatest/util/StringUtilTest.java
@@ -3,8 +3,17 @@ package io.espinoleandroo.javatest.util;
 public class StringUtilTest {
 
     public static void main(String[] args) {
-        String result = StringUtil.repeat("Hoola", 3);
+        String result;
+        result = StringUtil.repeat("Hoola", 3);
+        assertEquals(result, "HoolaHoolaHoola");
+        result = StringUtil.repeat("Hoola", 1);
+        assertEquals(result, "Hoola");
 
-        System.out.println(result);
+    }
+
+    private static void assertEquals(String actual, String expected) {
+        if(!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equeal to expected " + expected);
+        }
     }
 }
