17ded58260f3598d6c0ffbc9eaa9aea9a9804b90
espinoleandroo <espinoleandroo@gmail.com>
2021-08-27 23:31:39 -0500

    Java Testing -- Análisis de los tests y mejoras

--- Testing/src/test/java/io/espinoleandroo/java/testing/payments/PaymentProcessorTest.java
+++ Testing/src/test/java/io/espinoleandroo/java/testing/payments/PaymentProcessorTest.java
@@ -2,29 +2,41 @@ package io.espinoleandroo.java.testing.payments;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class PaymentProcessorTest {
 
+	PaymentGateway paymentGateway;
+	PaymentProcessor paymentProcessor;
+
+	@Before
+	public void setup() {
+		paymentGateway = Mockito.mock(PaymentGateway.class);
+		paymentProcessor = new PaymentProcessor(paymentGateway);
+	}
+
 	@Test
 	public void payment_is_correct() {
-		PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+
 		Mockito.when(paymentGateway.requestPayment((PaymentRequest) Mockito.any()))
 				.thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-		PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
-		assertTrue(paymentProcessor.makePayment(1000));
+		boolean result = paymentProcessor.makePayment(1000);
+
+		assertTrue(result);
 	}
-	
+
 	@Test
 	public void payment_is_wrong() {
-		PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+
 		Mockito.when(paymentGateway.requestPayment((PaymentRequest) Mockito.any()))
 				.thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-		PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
-		assertFalse(paymentProcessor.makePayment(1000));
+		boolean result = paymentProcessor.makePayment(1000);
+
+		assertFalse(result);
 	}
 
 }
