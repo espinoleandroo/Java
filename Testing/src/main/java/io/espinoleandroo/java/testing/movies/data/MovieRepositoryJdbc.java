3a9aa5ac2f1284e171561f82e7f64408ee26a2cd
espinoleandroo <espinoleandroo@gmail.com>
2021-09-09 23:31:35 -0500

    Java Testing -- Test de integración con base de datos: guardar películas y búsqueda de películas individuales

--- Testing/src/main/java/io/espinoleandroo/java/testing/movies/data/MovieRepositoryJdbc.java
+++ Testing/src/main/java/io/espinoleandroo/java/testing/movies/data/MovieRepositoryJdbc.java
@@ -22,27 +22,25 @@ public class MovieRepositoryJdbc implements MovieRepository {
 	@Override
 	public Movie findById(long id) {
 		// TODO Auto-generated method stub
-		return null;
+		Object[] args = { id };
+
+		return jdbcTemplate.queryForObject("SELECT * FROM movies WHERE id = ?", args, movieMapper);
 	}
 
 	@Override
 	public Collection<Movie> findAll() {
-		
 
 		return jdbcTemplate.query("SELECT * FROM movies", movieMapper);
 	}
 
 	@Override
 	public void saveOrUpdate(Movie movie) {
-		// TODO Auto-generated method stub
+		
+		jdbcTemplate.update("INSERT INTO movies (id, name, minutes, genre) values (?, ?, ?,)", movie.getId(), movie.getName(), movie.getMinutes(), movie.getGenre().toString());
 
 	}
-	
-	private static RowMapper<Movie> movieMapper = (rs, rowNum) ->
-    new Movie(
-        rs.getInt("id"),
-        rs.getString("name"),
-        rs.getInt("minutes"),
-        Genre.valueOf(rs.getString("genre")));
+
+	private static RowMapper<Movie> movieMapper = (rs, rowNum) -> new Movie(rs.getInt("id"), rs.getString("name"),
+			rs.getInt("minutes"), Genre.valueOf(rs.getString("genre")));
 
 }
