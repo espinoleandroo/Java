faf030610e10c6879265193ccf884653b67493c2
espinoleandroo <espinoleandroo@gmail.com>
2021-09-11 22:11:56 -0500

    Java Functional - Predicate

--- Functional/src/main/java/io/espinoleandroo/java/functional/MathFunctions.java
+++ Functional/src/main/java/io/espinoleandroo/java/functional/MathFunctions.java
@@ -1,11 +1,12 @@
 package io.espinoleandroo.java.functional;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class MathFunctions {
 
 	public static void main(String[] args) {
-		
+
 		Function<Integer, Integer> square = new Function<Integer, Integer>() {
 
 			@Override
@@ -13,8 +14,32 @@ public class MathFunctions {
 				return x * x;
 			}
 		};
+
+		System.out.println(square.apply(7));
+		System.out.println(square.apply(23));
 		
-		System.out.println(square.apply(5));
-		System.out.println(square.apply(25));
+		Function<Integer, Boolean> isOdd = x -> x % 2 == 1;
+		System.out.println(isOdd.apply(11));
+		
+		Predicate<Integer> isEven =  x -> x % 2 == 0;
+		System.out.println(isEven.test(16));
+		
+		Predicate<Student> isApproved = student -> student.getCalificacion() > 6.0;
+		Student leandro = new Student(10.0);
+		System.out.println(isApproved.test(leandro));
+	}
+	
+	static class Student {
+		private double calificacion;
+
+		public Student(double calificacion) {
+			super();
+			this.calificacion = calificacion;
+		}
+
+		public double getCalificacion() {
+			return calificacion;
+		}
+		
 	}
 }
