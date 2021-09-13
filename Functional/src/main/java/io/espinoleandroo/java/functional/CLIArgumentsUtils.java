6488cb78404354233df70227b3400c0dcc3f4d08
espinoleandroo <espinoleandroo@gmail.com>
2021-09-12 22:33:28 -0500

    Java Functional - Consumer y Supplier

---
+++ Functional/src/main/java/io/espinoleandroo/java/functional/CLIArgumentsUtils.java
@@ -0,0 +1,23 @@
+package io.espinoleandroo.java.functional;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class CLIArgumentsUtils {
+
+	static void showHelp(CLIArguments cliArguments) {
+		Consumer<CLIArguments> consumerHelper = cliArguments1 -> {
+			if (cliArguments.isHelp()) {
+				System.out.println("Manual solicitado");
+			}
+		};
+
+		consumerHelper.accept(cliArguments);
+	}
+
+	static CLIArguments generateCLI() {
+		Supplier<CLIArguments> generator = () -> new CLIArguments();
+
+		return generator.get();
+	}
+}
