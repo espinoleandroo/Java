8e12e4bc3ab59f0463468d1de9c155e8410008ec
espinoleandroo <espinoleandroo@gmail.com>
2021-09-15 19:23:31 -0500

    Java Functional - metodos default en nuestras interfaces

--- Functional/src/main/java/io/espinoleandroo/java/functional/StringFunctions.java
+++ Functional/src/main/java/io/espinoleandroo/java/functional/StringFunctions.java
@@ -16,6 +16,34 @@ public class StringFunctions {
 
 		BiFunction<String, Integer, String> leftPad = (text, number) -> String.format("%" + number + "s", text);
 		System.out.println(leftPad.apply("Java", 10));
+		
+		StringOperation six = () -> 6;
+		six.operate("Alumno");
+		
+		DoOperation operateFive = text -> System.out.println(text);
+		operateFive.execute(5, "ESTUDIANTE");
+	}
+	
+	@FunctionalInterface
+	interface DoOperation{
+		void take(String text);
+		
+		default void execute(int x, String text) {
+			while (x-- > 0) {
+				take(text);
+			}
+		}
 	}
 
+	@FunctionalInterface
+	interface StringOperation {
+		int getAmount();
+
+		default void operate(String text) {
+			int x = getAmount();
+			while (x-- > 0) {
+				System.out.println(text);
+			}
+		}
+	}
 }
