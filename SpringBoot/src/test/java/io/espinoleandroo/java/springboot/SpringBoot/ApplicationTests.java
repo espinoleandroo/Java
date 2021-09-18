522e10d956dc8f29d539df09c5b69ef12c763ddf
espinoleandroo <espinoleandroo@gmail.com>
2021-09-17 22:11:35 -0500

    Inicio Curso spring boot - Creación de proyecto

---
+++ SpringBoot/src/test/java/io/espinoleandroo/java/springboot/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.espinoleandroo.java.springboot.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
