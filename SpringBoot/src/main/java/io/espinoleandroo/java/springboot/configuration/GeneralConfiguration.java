9fd78733574eb17838084b571730bb7e32e7a637
espinoleandroo <espinoleandroo@gmail.com>
2021-09-19 22:20:37 -0500

    Java Spring Boot - Configuración de datasource con properties y classes

--- SpringBoot/src/main/java/io/espinoleandroo/java/springboot/configuration/GeneralConfiguration.java
+++ SpringBoot/src/main/java/io/espinoleandroo/java/springboot/configuration/GeneralConfiguration.java
@@ -1,7 +1,10 @@
 package io.espinoleandroo.java.springboot.configuration;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,5 +27,15 @@ public class GeneralConfiguration {
 	@Bean
 	public MyBeanWithProperties function() {
 		return new MyBeanWithPropertiesImplement(nombre, apellido);
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+		dataSourceBuilder.driverClassName("org.h2.Driver");
+		dataSourceBuilder.url("jdbc:h2:mem:testdb");
+		dataSourceBuilder.username("SA");
+		dataSourceBuilder.password("");
+		return dataSourceBuilder.build();
 	}
 }
