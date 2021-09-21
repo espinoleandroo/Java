1a87c88c6326db89611fa01f712e3e3654042503
espinoleandroo <espinoleandroo@gmail.com>
2021-09-20 23:35:08 -0500

    Java Spring Boot - anotación value para apuntar a properties

--- SpringBoot/src/main/java/io/espinoleandroo/java/springboot/configuration/GeneralConfiguration.java
+++ SpringBoot/src/main/java/io/espinoleandroo/java/springboot/configuration/GeneralConfiguration.java
@@ -7,12 +7,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import io.espinoleandroo.java.springboot.bean.MyBeanWithProperties;
 import io.espinoleandroo.java.springboot.bean.MyBeanWithPropertiesImplement;
 import io.espinoleandroo.java.springboot.dao.User;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 @EnableConfigurationProperties(User.class)
 public class GeneralConfiguration {
 	@Value("${value.nombre}")
@@ -24,6 +26,18 @@ public class GeneralConfiguration {
 	@Value("${value.random}")
 	private String random;
 	
+	@Value("${jdbc.url}")
+	private String jdbcUrl;
+	
+	@Value("${driver}")
+	private String driver;
+	
+	@Value("${username}")
+	private String username;
+	
+	@Value("${password}")
+	private String password;
+	
 	@Bean
 	public MyBeanWithProperties function() {
 		return new MyBeanWithPropertiesImplement(nombre, apellido);
@@ -32,10 +46,10 @@ public class GeneralConfiguration {
 	@Bean
 	public DataSource dataSource() {
 		DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-		dataSourceBuilder.driverClassName("org.h2.Driver");
-		dataSourceBuilder.url("jdbc:h2:mem:testdb");
-		dataSourceBuilder.username("SA");
-		dataSourceBuilder.password("");
+		dataSourceBuilder.driverClassName(driver);
+		dataSourceBuilder.url(jdbcUrl);
+		dataSourceBuilder.username(username);
+		dataSourceBuilder.password(password);
 		return dataSourceBuilder.build();
 	}
 }
