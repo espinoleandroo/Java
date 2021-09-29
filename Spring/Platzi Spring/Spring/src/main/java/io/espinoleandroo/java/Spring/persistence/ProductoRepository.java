276942e5c6b291935ea4322e52e69588682cf400
espinoleandroo <espinoleandroo@gmail.com>
2021-09-28 21:56:09 -0500

    Java Spring - Query Methods

--- Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/ProductoRepository.java
+++ Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/ProductoRepository.java
@@ -1,6 +1,7 @@
 package io.espinoleandroo.java.Spring.persistence;
 
 import java.util.List;
+import java.util.Optional;
 
 import io.espinoleandroo.java.Spring.persistence.crud.ProductoCrudRepository;
 import io.espinoleandroo.java.Spring.persistence.entity.Producto;
@@ -11,5 +12,13 @@ public class ProductoRepository {
 	
 	public List<Producto> getAll(){
 		return (List<Producto>) productoCrudRepository.findAll();
+	}
+	
+	public List<Producto> getByCategoria(int idCategoria){
+		return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+	}
+	
+	public Optional<List<Producto>> getEscasos(int cantidad){
+		return productoCrudRepository.findByCatidadStockLessThanAndEstado(cantidad, true);
 	}
 }
