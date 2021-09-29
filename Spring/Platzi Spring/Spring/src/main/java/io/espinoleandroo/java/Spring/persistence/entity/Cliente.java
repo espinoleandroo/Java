5b9c1bd4cf38304c079e5490a32329544d62c745
espinoleandroo <espinoleandroo@gmail.com>
2021-09-28 21:29:43 -0500

    Java Spring - clave primaria es compuesta

--- Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/entity/Cliente.java
+++ Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/entity/Cliente.java
@@ -16,10 +16,60 @@ public class Cliente {
 	
 	private String apellidos;
 	
-	private String celular;
+	private Long celular;
 	
 	private String direccion;
 	
 	@Column(name = "correo_electronico")
 	private String correoElectronico;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Long getCelular() {
+		return celular;
+	}
+
+	public void setCelular(Long celular) {
+		this.celular = celular;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+	
+	
 }
