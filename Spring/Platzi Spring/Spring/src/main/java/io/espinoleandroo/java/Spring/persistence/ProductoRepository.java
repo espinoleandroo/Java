686a8e090ffe87f13f0a0aba718b4af47436ff85
espinoleandroo <espinoleandroo@gmail.com>
2021-09-29 20:20:01 -0500

    Java Spring - anotación @Repository

--- Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/ProductoRepository.java
+++ Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/ProductoRepository.java
@@ -3,9 +3,12 @@ package io.espinoleandroo.java.Spring.persistence;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Repository;
+
 import io.espinoleandroo.java.Spring.persistence.crud.ProductoCrudRepository;
 import io.espinoleandroo.java.Spring.persistence.entity.Producto;
 
+@Repository
 public class ProductoRepository {
 
 	private ProductoCrudRepository productoCrudRepository;
@@ -20,5 +23,17 @@ public class ProductoRepository {
 	
 	public Optional<List<Producto>> getEscasos(int cantidad){
 		return productoCrudRepository.findByCatidadStockLessThanAndEstado(cantidad, true);
+	}
+	
+	public Optional<Producto> getProducto(int idProducto){
+		return productoCrudRepository.findById(idProducto);
+	}
+	
+	public Producto save(Producto producto) {
+		return productoCrudRepository.save(producto);
+	}
+	
+	public void delete(int idProducto) {
+		productoCrudRepository.deleteById(idProducto);
 	}
 }
