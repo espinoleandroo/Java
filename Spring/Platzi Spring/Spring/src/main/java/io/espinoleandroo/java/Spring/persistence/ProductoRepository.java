d55a2f53c189963aec1556d601dcaca128879dc9
espinoleandroo <espinoleandroo@gmail.com>
2021-09-30 20:36:43 -0500

    Java Spring - Orientar a dominio

--- Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/ProductoRepository.java
+++ Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/ProductoRepository.java
@@ -5,35 +5,49 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
+import io.espinoleandroo.java.Spring.domain.Product;
+import io.espinoleandroo.java.Spring.domain.repository.ProductRepository;
 import io.espinoleandroo.java.Spring.persistence.crud.ProductoCrudRepository;
 import io.espinoleandroo.java.Spring.persistence.entity.Producto;
+import io.espinoleandroo.java.Spring.persistence.mapper.ProductMapper;
 
 @Repository
-public class ProductoRepository {
+public class ProductoRepository implements ProductRepository{
 
 	private ProductoCrudRepository productoCrudRepository;
+	private ProductMapper mapper;
 	
-	public List<Producto> getAll(){
-		return (List<Producto>) productoCrudRepository.findAll();
+	@Override
+	public List<Product> getAll(){
+		List<Producto> productos = (List<Producto>) productoCrudRepository.findAll(); 
+		return mapper.toProducts(productos); 
 	}
 	
-	public List<Producto> getByCategoria(int idCategoria){
-		return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
-	}
-	
-	public Optional<List<Producto>> getEscasos(int cantidad){
-		return productoCrudRepository.findByCatidadStockLessThanAndEstado(cantidad, true);
-	}
-	
-	public Optional<Producto> getProducto(int idProducto){
-		return productoCrudRepository.findById(idProducto);
-	}
-	
-	public Producto save(Producto producto) {
-		return productoCrudRepository.save(producto);
-	}
-	
+	@Override
 	public void delete(int idProducto) {
 		productoCrudRepository.deleteById(idProducto);
+	}
+
+	@Override
+	public Optional<List<Product>> getByCategory(int idCategory) {
+		List<Producto> productos = productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategory); 
+		return Optional.of(mapper.toProducts(productos));
+	}
+
+	@Override
+	public Optional<List<Product>> getScarseProducts(int quantity) {
+		Optional<List<Producto>> productos = productoCrudRepository.findByCatidadStockLessThanAndEstado(quantity, true);
+		return productos.map(prods -> mapper.toProducts(prods));
+	}
+
+	@Override
+	public Optional<Product> getProduct(int productId) {
+		return productoCrudRepository.findById(productId).map(prod -> mapper.toProduct(prod));
+	}
+
+	@Override
+	public Product save(Product product) {
+		Producto producto = mapper.toProducto(product);
+		return mapper.toProduct(productoCrudRepository.save(producto));
 	}
 }
--- Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/mapper/ProductMapper.java
+++ Spring/Platzi Spring/Spring/src/main/java/io/espinoleandroo/java/Spring/persistence/mapper/ProductMapper.java
@@ -27,6 +27,6 @@ public interface ProductMapper {
 	
 	@InheritInverseConfiguration
 	@Mapping(target = "codigoBarras", ignore = true)
-	Producto toProduct(Product product);
+	Producto toProducto(Product product);
 	
 }
