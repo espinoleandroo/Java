f0134d4359c0ba904febe911893f0600f66eb864
espinoleandroo <espinoleandroo@gmail.com>
2021-10-02 19:46:21 -0500

    Java Spring - Spring Security

---
+++ Spring/Platzi Spring/SpringMaven/src/main/java/io/espinoleandroo/java/Spring/web/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package io.espinoleandroo.java.Spring.web.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import io.espinoleandroo.java.Spring.domain.service.UserDetailsService;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+
+	
+}
