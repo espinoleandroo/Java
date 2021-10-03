aa7410aaee85060518b4d7d225eada0e12759e87
espinoleandroo <espinoleandroo@gmail.com>
2021-10-02 21:58:08 -0500

    Java Spring - Deploy

--- Spring/Platzi Spring/SpringMaven/src/main/java/io/espinoleandroo/java/Spring/web/security/SecurityConfig.java
+++ Spring/Platzi Spring/SpringMaven/src/main/java/io/espinoleandroo/java/Spring/web/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,12 +45,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		return super.authenticationManagerBean();
 	}
 	
-	@Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
-                "/swagger-resources/**", "/configuration/security",
-                "/swagger-ui.html", "/webjars/**");
-    }
+//	@Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
+//                "/swagger-resources/**", "/configuration/security",
+//                "/swagger-ui.html", "/webjars/**");
+//    }
 
 	
 }
