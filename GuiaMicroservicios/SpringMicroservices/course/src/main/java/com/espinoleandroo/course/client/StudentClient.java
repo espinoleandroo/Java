2b2dffe49aa29b39d819280fb1d1d5d5cea8a211
Leandro Espino <espinoleandroo@gmail.com>
2023-12-13 21:49:51 -0600

    Configuración microservicio gateway

--- GuiaMicroservicios/SpringMicroservices/course/src/main/java/com/espinoleandroo/course/client/StudentClient.java
+++ GuiaMicroservicios/SpringMicroservices/course/src/main/java/com/espinoleandroo/course/client/StudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "localhost:8090/api/student/")
+@FeignClient(name = "msvc-student", url = "localhost:8080/api/student/")
 public interface StudentClient {
 
     @GetMapping("/search-by-course/{idCourse}")
